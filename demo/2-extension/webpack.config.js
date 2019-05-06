9c11a7ae62e323a3285e43c59cac477a364fc3e5
Francois Wouts <francois.wouts@airtasker.com>
2019-05-06 16:28:02 +1000

    Add steps to make it an extension

---
+++ demo/2-extension/webpack.config.js
@@ -0,0 +1,29 @@
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    popup: "./src/popup.jsx"
+  },
+  output: {
+    filename: "[name].js"
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        include: __dirname + "/src",
+        loader: "babel-loader",
+        test: /\.jsx?$/
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/popup.html",
+      inject: true,
+      chunks: ["popup"]
+    }),
+    new CopyPlugin([{ from: "manifest.json", to: "." }])
+  ]
+};
