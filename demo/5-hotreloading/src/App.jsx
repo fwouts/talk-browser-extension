f92457f6e1caf1e285a4655fab39a565c8d7cc90
Francois Wouts <francois.wouts@airtasker.com>
2019-05-07 13:18:36 +1000

    Set up hot reloading

---
+++ demo/5-hotreloading/src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from "react";
+import { hot } from "react-hot-loader/root";
+import { chrome } from "./chrome";
+
+const App = () => {
+  const [state, setState] = useState({
+    greeting: "Hello, World!"
+  });
+  useEffect(() => {
+    const listener = message => setState(message);
+    chrome.runtime.onMessage.addListener(listener);
+    return () => chrome.runtime.onMessage.removeListener(listener);
+  }, []);
+  return <div>{state.greeting}</div>;
+};
+export default hot(App);
