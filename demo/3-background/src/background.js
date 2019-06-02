5d95fe7364758bc39ca4b1c745f195d0d2161b18
Francois Wouts <francois.wouts@airtasker.com>
2019-06-03 06:21:49 +1000

    Separate demo into its own repo

--- demo/3-background/src/background.js
+++
@@ -1,7 +0,0 @@
-import randomName from "node-random-name";
-
-setInterval(() => {
-  chrome.runtime.sendMessage({
-    greeting: `Hello ${randomName()}`
-  });
-}, 1000);
--- demo/4-devserver/src/background.js
+++
@@ -1,8 +0,0 @@
-import randomName from "node-random-name";
-import { chrome } from "./chrome";
-
-setInterval(() => {
-  chrome.runtime.sendMessage({
-    greeting: `Hello ${randomName()}`
-  });
-}, 1000);
